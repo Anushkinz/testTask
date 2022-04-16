33600338735c8afc667b12caf7dbbed6f1e85bd5
Anushkinz <anuarprogramm@gmail.com>
2022-04-16 16:25:32 +0600

    Initial Commit

---
+++ testTask/testTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  testTaskApp.swift
+//  testTask
+//
+//  Created by anushkinz on 16/4/22.
+//
+
+import SwiftUI
+
+@main
+struct testTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
